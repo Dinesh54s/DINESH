20be7a1ba9885d02f278c4c5b1c63d1a28a6c29e
prabhu <prabhu@dinesh09subramani@gmail.com>
2022-09-20 20:54:23 +0530

    largest Number A or B

--- src/dhinesh/Testing.java
+++ src/dhinesh/Testing.java
@@ -2,9 +2,44 @@ package dhinesh;
 
 public class Testing {
 	public static void main(String args[]) {
-        String binarystring="101";
-       int decimal=Integer.parseInt(binarystring,2);
-        System.out.print(decimal);
+		// print pattern like 12121
+ for(int i=0;i<5;i++)
+           {
+           for(int j=0;j<=6;j++)
+           {
+            	if((j%2) == 0)
+            	{	
+            	System.out.print("1");
+                }
+            	else
+            		if((j%2) == 1)
+                	{	
+                	System.out.print("2");
+                    }
+            }
+            System.out.println();
+            }
 	}
 }
+	
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
 
