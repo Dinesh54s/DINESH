10db1862dc7125d1c835cb14fc2f1d79d23be16d
prabhu <prabhu@prabhu>
2022-10-05 22:08:56 +0530

    Maths

---
+++ src/DINESH/Maths.java
@@ -0,0 +1,36 @@
+package DINESH;
+
+public class Maths{
+        
+        	 int a,b;
+        	 void add()
+        	 {
+        		 System.out.println(a+b);
+        	 }
+        	 void sub()
+        	 {
+        		 System.out.println(a-b);
+        	 }
+        	 void mul()
+        	 {
+        		 System.out.println(a*b);
+        	 }
+        	 void div()
+        	 {
+        		 System.out.println(a/b);
+        		 
+        	 }
+  public static void main(String args[]) {
+	  
+		  Maths m1=new Maths ();
+		  m1.a=5;
+		  m1.b=3;
+		  m1.add();
+		  m1.sub();
+		  m1.mul();
+		  m1.div();
+	  
+	  }
+}
+
+  
